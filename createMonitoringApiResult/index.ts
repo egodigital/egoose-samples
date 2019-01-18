f2c046a415fdc4660f02b32ec35cb74b89817ca2
Marcel Kloubert <marcel.kloubert@gmx.net>
2019-01-18 15:45:20 +0100

    typos

--- createMonitoringApiResult/index.ts
+++ createMonitoringApiResult/index.ts
@@ -1,19 +1,20 @@
-/* 
- * This file is part of the XXX distribution (https://github.com/xxxx or http://xxx.github.io).
- * Copyright (c) 2015 Liviu Ionescu.
- * 
- * This program is free software: you can redistribute it and/or modify  
- * it under the terms of the GNU General Public License as published by  
+/*
+ * This file is part of the egoose-samples distribution (https://github.com/egodigital/egoose-samples).
+ * Copyright (c) e.GO Digital GmbH (https://www.e-go-digital.com/).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 3.
  *
- * This program is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
+ * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 
 import * as egoose from '@egodigital/egoose';
 import { table } from 'table';
--- logger/index.ts
+++ logger/index.ts
@@ -1,6 +1,6 @@
 /*
- * This file is part of the XXX distribution (https://github.com/xxxx or http://xxx.github.io).
- * Copyright (c) 2015 Liviu Ionescu.
+ * This file is part of the egoose-samples distribution (https://github.com/egodigital/egoose-samples).
+ * Copyright (c) e.GO Digital GmbH (https://www.e-go-digital.com/).
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
+
 
 import * as egoose from '@egodigital/egoose';
 
